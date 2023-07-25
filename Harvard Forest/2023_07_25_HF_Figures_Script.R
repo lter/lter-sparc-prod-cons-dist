52fa81257a0a161198426e91f6e14f9acd801dfc
camalmborg <malmborg@bu.edu>
2023-07-25 19:50:31 -0400

    adding lines for importing landsat forest condition and tcg data for time series plots

--- Harvard Forest/2023_07_25_HF_Figures_Script.R
+++ Harvard Forest/2023_07_25_HF_Figures_Script.R
@@ -16,15 +16,21 @@ library(tactile)
 
 ### MAKING SAMPLE TIME SERIES PLOTS:
 
+# loading Landsat-based remote sensing data for forest condition scores (cs) and Tasseled Cap Greenness Index (tcg):
+cs_file <- "Harvard Forest/HF_2021_Paper_Data/2023_07_25_LTER_plots_monthly_score_mean.csv"
+tcg_file <- "Harvard Forest/HF_2021_Paper_Data/2023_07_25_LTER_plots_monthly_tcg_mean.csv"
+# load csvs:
+cs <- read.csv(cs_file)
+tcg <- read.csv(tcg_file)
+
 # Harvard Forest data - Landsat time series at some of the sites:
-site <- 166
-#tcg <- tcg.values
-tcg <- cond.scores
+site <- 2 # 12 sites in data collection, pick 1-12
+obs <- cs # data observations - tcg or cs file from above - tcg for TCG, cs for condition scores
 
 #get just the tcg values from data frame:
-tcgs<-tcg[,c(grep("^X",colnames(tcg)))]
+time_series<-obs[,c(grep("^X",colnames(obs)))]
 
-y <- as.numeric((tcgs[site,32:80])/1000) #currently Apr 2011-Aug 2020
+y <- as.numeric(time_series[site,],) #currently Apr 2011-Aug 2020
 x <- 1:length(y)
 
 # R PLOT VERSION:---------------------
@@ -40,22 +46,22 @@ x <- 1:length(y)
 
 sample_site <- data.frame(
   month = x,
-  score = y
+  value = y/1000 # corrects for scaling in data from GEE product
 )
-colnames(sample_site) <- c("x","score")
+colnames(sample_site) <- c("x","value")
 
 ##GGPLOT VERSION ----------------------
 ##turn this stuff on and change name to save:
 #fname = "2023_07_20_Slow_Recov_sample_plot_166.tiff"
 #tiff(fname, units = "in", width=12, height=3, res=300)
-ggplot(data = sample_site, mapping = aes(x = x, y = score)) +
+ggplot(data = sample_site, mapping = aes(x = x, y = value)) +
   geom_line(color="forestgreen") +
   geom_point(color="forestgreen") + 
-  geom_line(data = filter(sample_site, is.na(score)==FALSE), 
+  geom_line(data = filter(sample_site, is.na(value)==FALSE), 
             linetype = "dashed", color="forestgreen", size=0.3) +
-  ylim(c(-2.2, 1.7)) +
-  labs(title = "Sample Forest Condition(April 2011-August 2020): Slow Recovery", 
-       y="Condition Score",
+  ylim(c(-3.2, 2)) +
+  labs(title = "Sample Forest Condition",
+       y="Condition Score", #or TCG - remember to change when making plots
        x="Growing Season Month") +
   scale_x_continuous(breaks = seq(min(x), max(x), by = 5)) +
   theme_bw() + theme(panel.border = element_blank(), 
