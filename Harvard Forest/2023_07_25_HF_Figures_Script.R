3bb9df5d9185699d643fd811818953ae5c34a945
camalmborg <malmborg@bu.edu>
2023-07-25 20:28:11 -0400

    removing unnecessary code, running time series plots for examples 7/25

--- Harvard Forest/2023_07_25_HF_Figures_Script.R
+++ Harvard Forest/2023_07_25_HF_Figures_Script.R
@@ -24,25 +24,14 @@ cs <- read.csv(cs_file)
 tcg <- read.csv(tcg_file)
 
 # Harvard Forest data - Landsat time series at some of the sites:
-site <- 2 # 12 sites in data collection, pick 1-12
+site <- 12 # 12 sites in data collection, pick 1-12
 obs <- cs # data observations - tcg or cs file from above - tcg for TCG, cs for condition scores
 
 #get just the tcg values from data frame:
 time_series<-obs[,c(grep("^X",colnames(obs)))]
 
-y <- as.numeric(time_series[site,],) #currently Apr 2011-Aug 2020
+y <- as.numeric(time_series[site,],31:80) #currently Apr 2011-Aug 2020
 x <- 1:length(y)
-
-# R PLOT VERSION:---------------------
-# #samp_time_series <- tcgjune[site,]
-# #par(mar=c(1,1,1,1))
-# plot(x, y, frame=FALSE,
-#      xlab = "Growing Season Month",
-#      ylab = "Condition Score",
-#      pch = 20,
-#      col = "forestgreen")#,
-#      #ylim = c(-6.75,4.5))
-# #lines(x, y, lwd = 1, col="forestgreen")
 
 sample_site <- data.frame(
   month = x,
@@ -72,25 +61,14 @@ ggplot(data = sample_site, mapping = aes(x = x, y = value)) +
   #       panel.grid.minor = element_blank())
 #dev.off()
 
-
-#testing subset sites:
-# distsites <- c(10,18,21,30,31,32,47,53,60,
-#                66,72,97,98,101,102,105,110,
-#                111,120,130,135,141,142,145,
-#                146,151,152,153,157,160,170,
-#                171,172,188,195,196,203)
-# distsdoub <- c(14,119,147,161,164,166,167,173,174,175,191,197)
-# fastrecov <- c(41,42,43,94,95)
-# slowrecov <- c(48,54,55,58,75,87)
-# 
-# ds <- tcgs[distsites, 21:79]
-# dd <- tcgs[distsdoub, 21:79]
-# fr <- tcgs[fastrecov, 21:79]
-# sr <- tcgs[slowrecov, 21:79]
-# 
-# ds_mr <- hf_data[distsites,]
-# dd_mr <- hf_data[distsdoub,]
-# fr_mr <- hf_data[fastrecov,]
-# sr_mr <- hf_data[slowrecov,]
-
+# R PLOT VERSION:---------------------
+# #samp_time_series <- tcgjune[site,]
+# #par(mar=c(1,1,1,1))
+# plot(x, y, frame=FALSE,
+#      xlab = "Growing Season Month",
+#      ylab = "Condition Score",
+#      pch = 20,
+#      col = "forestgreen")#,
+#      #ylim = c(-6.75,4.5))
+# #lines(x, y, lwd = 1, col="forestgreen")
 
