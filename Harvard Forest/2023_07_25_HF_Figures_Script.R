dbcf149cc37b3a0d60865dafe583e1d6774e28f6
camalmborg <malmborg@bu.edu>
2023-07-26 19:46:21 -0400

    making time series of recovery with missing observations (non-growing season months)

--- Harvard Forest/2023_07_25_HF_Figures_Script.R
+++ Harvard Forest/2023_07_25_HF_Figures_Script.R
@@ -4,6 +4,8 @@
 library(tidyverse)
 library(ggplot2)
 library(dplyr)
+library(stringr)
+library(lubridate)
 library(mgcv)
 library(pROC)
 library(ggforce)
@@ -30,12 +32,37 @@ obs <- cs # data observations - tcg or cs file from above - tcg for TCG, cs for 
 #get just the tcg values from data frame:
 time_series<-obs[,c(grep("^X",colnames(obs)))]
 
-y <- as.numeric(time_series[site,],31:80) #currently Apr 2011-Aug 2020
-x <- 1:length(y)
+#remove unnecessary parts of colnames to just get dates:
+colnames(time_series) <- str_replace_all(names(time_series), c("X"="", 
+                                                               "_score_mean"="", 
+                                                               "_tcg_mean"="",
+                                                               "\\."="-"))
+
+#y <- as.numeric(time_series[site,31:80]) #currently Apr 2011-Aug 2020
+#y <- recov <- time_series[site,c(first(grep("^2017", colnames(time_series))):ncol(time_series))]
+recov <- time_series[site,c(first(grep("^2017", colnames(time_series))):ncol(time_series))]
+
+### Adding dummy data to time series of recovery (for months with no observations)
+# getting all months from beginning to end of recovery:
+allmonths <- as.character(
+  seq.Date(from = as.Date(first(names(recov))),
+  to = as.Date(last(names(recov))),
+  by="month"))
+#selecting months with no observations:
+no_obs_mos <- symdiff(names(recov), allmonths)
+#make dummy dataframe with correct dimensions:
+no_obs <- data.frame(matrix(data = NA, nrow = nrow(recov), ncol = length(no_obs_mos)))
+#add the column names to sort by date during cbind:
+colnames(no_obs) <- c(no_obs_mos)
+#cbind for time series:
+
+
+#x <- 1:length(y)
+# TO DO: NEED TO ADD NON-DATA MONTHS TO TIME SERIES 
 
 sample_site <- data.frame(
-  month = x,
-  value = y/1000 # corrects for scaling in data from GEE product
+  month = colnames(y),
+  value = y # corrects for scaling in data from GEE product
 )
 colnames(sample_site) <- c("x","value")
 
@@ -48,8 +75,8 @@ ggplot(data = sample_site, mapping = aes(x = x, y = value)) +
   geom_point(color="forestgreen") + 
   geom_line(data = filter(sample_site, is.na(value)==FALSE), 
             linetype = "dashed", color="forestgreen", size=0.3) +
-  ylim(c(-3.2, 2)) +
-  labs(title = "Sample Forest Condition",
+  #ylim(c(-3.2, 2)) +
+  labs(title = "Sample Forest Condition: Recovery",
        y="Condition Score", #or TCG - remember to change when making plots
        x="Growing Season Month") +
   scale_x_continuous(breaks = seq(min(x), max(x), by = 5)) +
