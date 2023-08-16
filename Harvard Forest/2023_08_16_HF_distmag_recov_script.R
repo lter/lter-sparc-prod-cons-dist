26a2fa17406401b040358c20b9a757ad2c85dd2e
camalmborg <malmborg@bu.edu>
2023-08-16 13:25:54 -0400

    quick calcs for means across sites if needed

--- Harvard Forest/2023_08_16_HF_distmag_recov_script.R
+++ Harvard Forest/2023_08_16_HF_distmag_recov_script.R
@@ -4,3 +4,10 @@ tcg_file <- "Harvard Forest/HF_2021_Paper_Data/2023_07_25_LTER_plots_monthly_tcg
 # load csvs:
 cs <- read.csv(cs_file)
 tcg <- read.csv(tcg_file)
+
+magsrec1 <- spongy_mpr(tcg, cs, 2016)
+magsrec2 <- spongy_mpr(tcg, cs, 2017)
+
+magsrec3 <- spongy_mpr(cs, cs, 2016) #uses forest cond scores and initial dist yr 2016
+magsrec4 <- spongy_mpr(cs, cs, 2017)
+
